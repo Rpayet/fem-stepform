2adfb1800ce444d6548a7a2eb74baa354b2fc854
Rpayet <robinpayet@gmail.com>
2025-06-06 21:27:27 +0200

    Refactor routing structure in App component and remove HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { HashRouter, Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SubscribeForm from './pages/SubscribeForm';
 import SubFormSteps from './pages/Forms/subscribe/SubFormSteps';
 
 export default function App() {
+
     return (
-        <HashRouter>
-            <div id="App">
-                <Routes>
-                    <Route index path='/' element={<Link to='/subscribe/step1'>Click here to begin</Link>} />
-                    <Route path='/subscribe' element={<SubscribeForm />}>
-                        <Route path='step1' element={<SubFormSteps />} />
-                        <Route path='step2' element={<SubFormSteps />} />
-                        <Route path='step3' element={<SubFormSteps />} />
-                        <Route path='step4' element={<SubFormSteps />} />
-                        <Route path='confirm' element={<SubFormSteps />} />
-                    </Route>    
-                </Routes>
-            </div>
-        </HashRouter>
+        <div id="App">
+            <Routes>
+                <Route index path='/fem-stepform' element={<Link to='/fem-stepform/subscribe/step1'>Click here to begin</Link>} />
+                <Route path='/fem-stepform/subscribe' element={<SubscribeForm />}>
+                    <Route path='step1' element={<SubFormSteps />} />
+                    <Route path='step2' element={<SubFormSteps />} />
+                    <Route path='step3' element={<SubFormSteps />} />
+                    <Route path='step4' element={<SubFormSteps />} />
+                    <Route path='confirm' element={<SubFormSteps />} />
+                </Route>    
+            </Routes>
+        </div>
     );
 }
+
